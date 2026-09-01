be40dd9bf914069c3316fc9e68fa3788139715cb
Abudukaiyumu yasen <ecedwdpf@outlook.com>
2026-09-01 11:19:18 +0800

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size(),m = board[0].size();
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(board[i][j] == word[0] && dfs(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, string word,int i,int j,int idx){
+        if(idx == word.size()) return true;
+        if(i<0 || i>= board.size() || j<0 || j >= board[0].size() || board[i][j] != word[idx]) return false;
+
+        char tmp = board[i][j];
+        board[i][j] = '#';
+        bool found = dfs(board,word,i,j+1,idx+1)||
+                     dfs(board,word,i,j-1,idx+1) ||
+                     dfs(board,word,i+1,j,idx+1) ||
+                     dfs(board,word,i-1,j,idx+1);
+        board[i][j] = tmp;
+        return found;
+    }
+};
