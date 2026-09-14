f47dcc1fd8a1524ed0700c0e84588e06f9d50a46
Abudukaiyumu yasen <ecedwdpf@outlook.com>
2026-09-15 07:49:48 +0800

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s){
+            if(c=='(' || c=='{'|| c=='[') st.push(c);
+            else if(c==')' || c==']' || c=='}'){
+                if(st.empty()) return false;
+                char t = st.top();st.pop();
+                if((t=='(' && c!=')') || (t=='{' && c!='}') || (t=='[' && c!=']')) return false; 
+            }
+        }
+        return st.empty();
+    }
+};
